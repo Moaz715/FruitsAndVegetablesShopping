ee8533857c235c30c9d38e759ea3b107c649437c
Moaz715 <moazfathy592005@gmail.com>
2025-07-29 13:41:49 +0300

    Create Cart.cs

---
+++ FruitsAndVegetablesShopping.DAL/Entities/Cart.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_Group7_demo_DAL.Entities
+{
+    public class Cart
+    {
+        public int Cart_id { get; private set; }
+
+        public int numOfItems { get; private set; }
+
+        [Required]
+        public DateTime CreatedOn { get; private set; } = DateTime.Now;
+
+        public string? CreatedBy { get; private set; }
+
+        public DateTime? ModifiedOn { get; private set; }
+
+        public string? ModifiedBy { get; private set; }
+
+        public bool isDeleted { get; private set; } = false;
+
+        public DateTime? DeletedOn { get; private set; }
+
+        public string? DeletedBy { get; private set; }
+
+        public void delete()
+        {
+            this.isDeleted = true;
+            this.DeletedOn = DateTime.Now;
+
+        }
+    }
+}
